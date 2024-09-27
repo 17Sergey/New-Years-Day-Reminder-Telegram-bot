baeb84bc88d596165b17e5e456ddc931def82f4a
Sergey <sergeykachaliukdev@gmail.com>
2024-09-27 10:53:50 +0300

    fix: add missing line

--- utils/handler.js
+++ utils/handler.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const BASE_URL = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
+
 export const axiosInstance = {
   get(url, params) {
     return axios.get(`/${url}`, {
